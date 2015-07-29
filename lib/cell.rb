4569c727816eb37ef132335f9c73e8507e436d7a
Rebecca Appleyard <rklappleyard@gmail.com>
2015-07-29 11:03:47 +0100

    Created Cell class with content

---
+++ lib/cell.rb
@@ -0,0 +1,9 @@
+class Cell
+
+attr_reader :content
+
+def add_content(content)
+  @content = content
+end
+
+end
---
+++ spec/cell_spec.rb
@@ -0,0 +1,8 @@
+require 'cell'
+
+describe Cell do
+
+  it "can have content" do
+    expect(subject).to respond_to(:add_content)
+  end
+end
