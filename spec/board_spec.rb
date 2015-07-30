89fbc2be78afdcc1551fb0e045599762ea10c421
Rebecca Appleyard <rklappleyard@gmail.com>
2015-07-30 12:07:47 +0100

    Wrote more tests for check coordinates

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -14,15 +14,23 @@ describe Board do
     it 'responds to place' do
       expect(subject).to respond_to(:place).with(2).argument
     end
+  end
+
+  describe "#check_coord" do
+    before(:each) do
+      allow(ship).to receive(:destroyer)
+    end
 
     it 'gives an error if attempts to place horizontal ship on grid' do
-      allow(ship).to receive(:destroyer)
       expect{subject.place(ship, 'J1')}.to raise_error "Your ship is off the grid"
     end
 
     it 'gives an error if attempts to place vertical ship on grid' do
-      allow(ship).to receive(:destroyer)
       expect{subject.place(ship, 'A10', :vertical)}.to raise_error "Your ship is off the grid"
+    end
+
+    it 'should return a grid reference for valid coordinates' do
+      expect(subject.check_coord(ship, [0, 0], :horizontal)).to eq(0)
     end
   end
 end
