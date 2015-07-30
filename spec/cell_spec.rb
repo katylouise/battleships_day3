599e91dd75979906f7e93bfc5573b0e59f7bfe74
Rebecca Appleyard <rklappleyard@gmail.com>
2015-07-30 12:20:50 +0100

    Added describe #change_contents to cell spec

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -8,14 +8,16 @@ describe Cell do
     expect(subject.content).not_to eq(Cell.new.content)
   end
 
-  it 'should put a ship in the cell' do
-    subject.change_contents(ship)
-    expect(subject.content).to eq(ship)
-  end
+  describe "#change_contents" do
+    it 'should put a ship in the cell' do
+      subject.change_contents(ship)
+      expect(subject.content).to eq(ship)
+    end
 
-  it "should not contain more than one ship" do
-    subject.content = ship
-    expect{ subject.change_contents(ship) }.to raise_error "Occupied"
+    it "should not contain more than one ship" do
+      subject.content = ship
+      expect{ subject.change_contents(ship) }.to raise_error "Occupied"
+    end
   end
 
   it "should be hit when fired at" do
